0e7a9d7ea7fbc1c02bf338e433a7c5b3cd5ffed2
Aksel <axel.akondegmail.com>
2026-01-05 21:57:08 +0100

    menu glissant sur la gauche trop stylé

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import {useAuth} from "@/hooks/useAuth";
 import HomeScreen from "@//app/(tabs)/index";
 import RegisterScreen from "@/app/(tabs)/register";
@@ -8,7 +8,7 @@ import CalendarScreen from "@/app/(tabs)/google-calendar";
 import LogoutScreen from "@/app/(tabs)/logout";
 import AboutScreen from "@/app/(tabs)/about";
 
-const TopTabs = createMaterialTopTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function TabsLayout() {
     const {isAuthenticated} = useAuth();
@@ -27,24 +27,23 @@ export default function TabsLayout() {
     ];
 
     return (
-        <TopTabs.Navigator
+        <Drawer.Navigator
             key={isAuthenticated ? "auth" : "noauth"}
             screenOptions={{
                 lazy: true,
-                tabBarIndicatorStyle: {backgroundColor: "#000"},
-                tabBarStyle: {backgroundColor: "#fff"},
-                tabBarLabelStyle: {fontWeight: "600"},
+                drawerPosition: "left", // menu à gauche
+                drawerType: "slide",
                 swipeEnabled: true,
             }}
         >
             {screens.map((s) => (
-                <TopTabs.Screen
+                <Drawer.Screen
                     key={s.name}
                     name={s.name}
                     component={s.component}
                     options={{title: s.title}}
                 />
             ))}
-        </TopTabs.Navigator>
+        </Drawer.Navigator>
     );
 }
